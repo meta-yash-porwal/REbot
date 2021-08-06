be41be517d55a2e3ed83be3dae857afc7065ddcb
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2021-08-06 15:56:27 +0530

    debugging

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -51,7 +51,7 @@ module.exports = controller => {
                 let teamIdsArray = reqBody.teamId.split(',');
                 console.log('team id array...', teamIdsArray);
                 const teams = await controller.plugins.database.teams.find({ id: { $in: teamIdsArray } });
-                
+                teams = null;
                 if (!teams) {
                     return logger.log('team not found for id:', reqBody.teamId);
                 } else {
