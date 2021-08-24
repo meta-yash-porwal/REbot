fdea0eae65345cc696ed4b708294fe2d01320ace
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2021-08-24 11:43:23 +0530

    add support of multi select content list

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -767,7 +767,7 @@ module.exports = controller => {
                                             "optional" : true,
                                             "block_id": "blkref",
                                             "element": {
-                                                "type": "static_select",
+                                                "type": "multi_static_select",
                                                 "action_id": "reftype_select",
                                                 "placeholder": {
                                                     "type": "plain_text",
