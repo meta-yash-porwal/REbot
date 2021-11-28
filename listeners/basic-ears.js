16158f0457822e3b7d7783d87606ec2362f8d07b
Rahul Sharma <38658846+RahulSharma92@users.noreply.github.com>
2021-11-28 19:54:17 +0530

    email issue handled.

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -805,7 +805,7 @@ module.exports = controller => {
                         
                         if (actionName == 'content_search') {
                             if(pvt_metadata.pkg_version < 2.26) {
-                                await opportunityFlow(bot, message, existingConn, actionName, null, null);
+                                await opportunityFlow(bot, message, existingConn, actionName, pvt_metadata.email, null);
                             } else{
                                 console.log('...view submission content opp flow....');
                                 let mapval = await getRefTypes(existingConn,actionName);
