e2842ebfe7a9c10edfdb4af5831765a911ad9ef9
Yash Porwal <yash.porwal@metacube.com>
2022-06-01 21:52:40 +0530

    update

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -2013,7 +2013,8 @@ module.exports = controller => {
                         pvt_metadata.isUpdateable = message.view.state.values.isUpdateableConBlock.isUpdateableCon.selected_options ?
                             message.view.state.values.isUpdateableConBlock.isUpdateableCon.selected_options.value :
                             false;
-                        console.log("MEssage view EARS 2016 ", message.view.state.values.isUpdateableConBlock);
+                        console.log("MEssage view EARS 2016 ", message.view.state.values.isUpdateableConBlock.isUpdateableCon.selected_options);
+                        console.log('pvt_metadata.isUpdateable', pvt_metadata);
                         // message.view.private_metadata = JSON.stringify(pvt_metadata);
                         // refUseRequestModalWithContactInfo(bot, message);
 
