3517f1f55c25292f7a3d6cf67bd83b69c850669b
Yash Porwal <yash.porwal@metacube.com>
2022-06-20 16:19:18 +0530

    update

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -3858,8 +3858,8 @@ module.exports = controller => {
                             let pvt_metadata = JSON.parse(message.view.private_metadata);
 
                             if (requestStatus == "Decline") {
-                                await bot.api.views.update({
-                                    view_id: message.view.id,
+                                bot.httpBody({
+                                    response_action: 'update',
                                     view: {
                                         "type": "modal",
                                         "callback_id": "AD_Modal",
@@ -3967,8 +3967,8 @@ module.exports = controller => {
                                 });
                             } else if (requestStatus == "Approve") {
 
-                                await bot.api.views.update({
-                                    view_id: message.view.id,
+                                bot.httpBody({
+                                    response_action: 'update',
                                     view: {
                                         "type": "modal",
                                         "callback_id": "AD_Modal",
