70eb6a61e7c269e8f7c69485e78b578023530de7
Yash Porwal <yash.porwal@metacube.com>
2022-06-27 11:43:24 +0530

    update

--- listeners/basic-ears.js
+++ listeners/basic-ears.js
@@ -1945,7 +1945,7 @@ module.exports = controller => {
                                         "blkCon1": "A contact must be selected before approving a request."
                                     }
                                 });
-                            } else if (message.view.state.values.blkCon2 && message.view.state.values.blkCon2.con_select1.selected_option) {
+                            } else if (message.view.state.values.blkCon2 && message.view.state.values.blkCon2.con_select2.selected_option) {
                                 bot.httpBody({
                                     "response_action": "errors",
                                     "errors": {
